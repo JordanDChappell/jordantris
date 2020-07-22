4290aec6042e82eedd24af967fb6054518f4795b
Jordan Chappell <Jordan.Chappell@newcastle.edu.au>
2020-07-23 06:57:24 +1000

    Add better comments to the base block class.

--- codebase/src/components/GameBlock/Block.js
+++ codebase/src/components/GameBlock/Block.js
@@ -8,13 +8,27 @@ export default class Block {
     this.rotationIndex = 0;
   }
 
+  // ################ Drawing functions ################ //
+
+  /**
+   * * Draw a single block used to build larger shapes. Draws the block using the local coordinates
+   * * as a negative origin (draws from top left to bottom right by current blockSize).
+   * @param {number} x | x-axis local coordinate to draw a block from
+   * @param {number} y | y-axis local coordinate to draw a block from
+   */
   drawBlock(x, y) {
     this.context.fillStyle = this.color;
-    this.context.strokeRect(x, y, this.blockSize, this.blockSize);
-    this.context.fillRect(x + 1, y + 1, this.blockSize - 2, this.blockSize - 2);
+    this.context.strokeRect(x, y, this.blockSize, this.blockSize);  // draw the blocks outline
+    this.context.fillRect(x + 1, y + 1, this.blockSize - 2, this.blockSize - 2);  // draw the blocks color, slightly smaller than the outline
   }
 
+  /**
+   * * Clear a single block from the canvas.
+   * @param {number} x | x-axis local coordinate to clear a block from
+   * @param {number} y | y-axis local coordinate to clear a block from 
+   */
   clearBlock(x, y) {
+    // * Clear a slightly larger area (1 pixel in all directions) around a block.
     this.context.clearRect(
       x - 1,
       y - 1,
@@ -23,20 +37,28 @@ export default class Block {
     );
   }
 
+  /**
+   * * Moves the origin of the block to a new place, handles grid transform from pixels to local
+   * * coordinate system.
+   * @param {number} x | x-axis coordinate in the grid
+   * @param {number} y | y-axis coordinate in the grid
+   */
   moveOrigin(x, y) {
     this.xOrigin = x * this.blockSize;
     this.yOrigin = y * this.blockSize;
   }
 
+  /**
+   * * Draw the current shape in the canvas, using combinations of squares.
+   */
   draw() {
-    console.log(this.rotationIndex);
-    console.log(this.shapeMatrix);
-    var shape = this.shapeMatrix[this.rotationIndex];
+    var shape = this.shapeMatrix[this.rotationIndex]; // takes the current rotation of our shape
 
+    // * Loop over the x and y axis in the shape matrix, drawing a block in a position marked with a 1
     for (let y = 0; y < shape.length; y++) {
       var row = shape[y];
       for (let x = 0; x < row.length; x++) {
-        if (row[x]) {
+        if (row[x]) { // only draw blocks where a 1 is set
           this.drawBlock(
             this.xOrigin + x * this.blockSize,
             this.yOrigin + y * this.blockSize
@@ -46,9 +68,13 @@ export default class Block {
     }
   }
 
+  /**
+   * * Clear the current shape from the canvas.
+   */
   clear() {
     var shape = this.shapeMatrix[this.rotationIndex];
 
+    // * Loop over the x and y axis in the shape matrix, clearing all of the blocks marked with a 1
     for (let y = 0; y < shape.length; y++) {
       var row = shape[y];
       for (let x = 0; x < row.length; x++) {
@@ -62,12 +88,22 @@ export default class Block {
     }
   }
 
+  // ############## Movement functions ################# //
+
+  /**
+   * * Helper function to clear a block and draw it in a new position.
+   * @param {number} x | x coordinate to move our shape to
+   * @param {number} y | y coordinate to move our shape to
+   */
   moveTo(x, y) {
     this.clear();
     this.moveOrigin(x, y);
     this.draw();
   }
 
+  /**
+   * * Helper function to clear a block and select a new rotation in the shape matrix.
+   */
   rotate() {
     this.clear(); // clear the shape before rotating, uses the current shape in the matrix to clear
     this.rotationIndex =

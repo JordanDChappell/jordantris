af619f862545e48dd77053a69c6a704c8d844017
Jordan Chappell <jordan.chappell33@gmail.com>
2020-09-28 06:43:25 +1000

    Add environment webpack configs.

--- codebase/webpack.common.js
+++ codebase/webpack.common.js
@@ -3,13 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(),

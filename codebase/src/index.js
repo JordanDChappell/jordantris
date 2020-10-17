11e71ea72c4e8e62e73e576aa8d7fb1d8338d632
Jordan Chappell <jordan.chappell33@gmail.com>
2020-10-17 12:00:35 +1100

    Implement a start button so that users can restart if required.

--- codebase/src/index.js
+++ codebase/src/index.js
@@ -1,21 +1,25 @@
 import { init, start } from './game';
+import { startButton } from './components/MenuItems/StartButton';
 
 import './styles/global/main.global.css';
 
 const containerId = 'game-container';
 
-function component() {
-  // * Create page components
-
+function gameContainer(id) {
   // * Create a container div for the game grid
   const container = document.createElement('div');
-  container.setAttribute('id', containerId);
+  container.setAttribute('id', id);
   container.setAttribute('class', 'flexbox center col game-container');
+
+  // * Add some menu item elements to the container
+  const playButton = startButton.render(start);
+
+  container.append(playButton);
+
   return container;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(gameContainer(containerId));
 
-// * Initialize the game into the created container and start the loop
+// * Initialize the game into the created container
 init(document.getElementById(containerId));
-start();

8ae7b30340f4628873617f63075c625cffcd806c
Jordan Chappell <jordan.chappell33@gmail.com>
2023-06-25 13:33:27 +1000

    Add image blocks back to source code

--- codebase/src/components/GameBlock/IBlock.js
+++ codebase/src/components/GameBlock/IBlock.js
@@ -1,36 +1,40 @@
 import Block from './Block';
+import Image1 from '../../images/i_block/block_1.gif';
+import Image2 from '../../images/i_block/block_2.gif';
+import Image3 from '../../images/i_block/block_3.gif';
+import Image4 from '../../images/i_block/block_4.gif';
 
 export default class IBlock extends Block {
   constructor(context, blockSize) {
-    super(context, blockSize, '#00FFFF');
-
+    super(context, blockSize, '#31ffff');
+    
     // Create images to display in each block of the shape
-    // var image1 = document.createElement('img');
-    // image1.src =
-    //   'https://images.jordanchappell.com/projects/jordantris/i_block/block_1.gif';
-    // var image2 = document.createElement('img');
-    // image2.src =
-    //   'https://images.jordanchappell.com/projects/jordantris/i_block/block_2.gif';
-    // var image3 = document.createElement('img');
-    // image3.src =
-    //   'https://images.jordanchappell.com/projects/jordantris/i_block/block_3.gif';
-    // var image4 = document.createElement('img');
-    // image4.src =
-    //   'https://images.jordanchappell.com/projects/jordantris/i_block/block_4.gif';
+    var image1 = document.createElement('img');
+    image1.src = Image1;
+    var image2 = document.createElement('img');
+    image2.src = Image2;
+    var image3 = document.createElement('img');
+    image3.src = Image3;
+    var image4 = document.createElement('img');
+    image4.src = Image4;
 
     this.shapeMatrix = [
       [
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0]
+        [image1, 0, 0, 0],
+        [image2, 0, 0, 0],
+        [image3, 0, 0, 0],
+        [image4, 0, 0, 0]
       ],
       [
-        [1, 1, 1, 1],
+        [image4, image3, image2, image1],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
       ]
     ];
+  }
+
+  static preLoadImages() {
+    Block.preLoadImages('iblock', [Image1, Image2, Image3, Image4]);
   }
 }
